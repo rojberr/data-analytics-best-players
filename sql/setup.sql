6cd86c7b379afccf9c6e91c8b5ccb55d0a58996f
rojberr <rojberr@outlook.com>
2022-05-18 18:24:10 +0200

    feat(db): Add setup.sql and bind it to docker image on start

---
+++ sql/setup.sql
@@ -0,0 +1,7 @@
+// setup.sql
+// there are more columns in AnimeList.csv but i am using a simplified dataset for now
+// Data may need to be cleaned further before import
+CREATE TABLE nba (
+  nba INT PRIMARY KEY,
+  title TEXT
+);
