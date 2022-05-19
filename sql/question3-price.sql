5c7e9ab64af17b44df2b0d22e8ccc5efb82ba96f
rojberr <rojberr@outlook.com>
2022-05-19 18:29:06 +0200

    feat(sql): Introduce Q3 - How much do they cost

--- sql/question3-price.sql
+++ sql/question3-price.sql
@@ -1,3 +1,7 @@
 -- Question 3
--- How much do those players cost? What is they price?  
-SELECT * FROM nba1718salary;
+-- How much do those players cost? What is they price?
+SELECT
+    PLAYER,
+    SEASON1718
+FROM nba1718salary
+    ORDER BY SEASON1718 DESC;
