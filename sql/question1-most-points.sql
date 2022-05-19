de1b0cb66b9fd94feea215f3711f406bc59995de
rojberr <rojberr@outlook.com>
2022-05-19 18:43:27 +0200

    feat(db): Create VIEWS in sql

--- sql/question1-most-points.sql
+++ sql/question1-most-points.sql
@@ -1,3 +1,10 @@
 -- Question one
 -- Which player scored the most points in 21th season?
-SELECT * FROM all_seasons WHERE season > '2021' ORDER BY pts DESC;
+SELECT * FROM all_seasons WHERE season > '2017' AND season < '2019' ORDER BY pts DESC;
+
+-- Create a view
+CREATE OR REPLACE VIEW most_points AS
+SELECT * FROM all_seasons WHERE season > '2017' AND season < '2019' ORDER BY pts DESC;
+
+-- Check view
+SELECT * FROM most_points;
--- sql/question3-price.sql
+++ sql/question3-price.sql
@@ -5,3 +5,14 @@ SELECT
     SEASON1718
 FROM nba1718salary
     ORDER BY SEASON1718 DESC;
+
+-- Create a view
+CREATE OR REPLACE VIEW price AS
+SELECT
+    PLAYER,
+    SEASON1718
+FROM nba1718salary
+    ORDER BY SEASON1718 DESC;
+
+-- Check view
+SELECT * FROM price;
